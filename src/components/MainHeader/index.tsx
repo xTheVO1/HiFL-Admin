862b55c6510b5b3614bee07c59109a381dfb2dad
oghenero <sigbenuoghenero@gmail.com>
2022-03-21 08:29:00 +0100

    updates on the main header

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,20 +1,21 @@
-// import { useState } from "react";
+import {MdNotifications, MdHome} from 'react-icons/md'
 import { Container, Welcome, Username, Profile } from "./styles";
 
 export default function MainHeader () {
-    // const handleChangeTheme = () => {
-    //     setDarkTheme(!darkTheme);
-    //     toggleTheme();
-    // }
 
     return (
         <Container>
             <div></div>
             {/* <Toggle checked={darkTheme} labelLeft="Light" labelRight="Dark" onChange={handleChangeTheme}/> */}
             <Profile>
-                <Welcome>Gabriel Valin</Welcome>
+                <Welcome><MdHome/> FUTA TIGERS <span></span></Welcome>
+                <Welcome><MdNotifications/> NOTIFICATIONS<span></span></Welcome>
                 <Username>
-                    Role
+                   <div className="profile-img"></div> 
+                   <div className="details">
+                       <h5>VICTOR OLAITAN</h5>
+                       <p>TEAM MANAGER</p>
+                   </div>
                 </Username>
             </Profile>
         </Container>
