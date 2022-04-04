230496f08d5f250ffc5614522f27756444ec3772
oghenero <sigbenuoghenero@gmail.com>
2022-04-04 12:05:25 +0100

    fixed get teams by team managers

--- src/baseUrl/index.ts
+++ src/baseUrl/index.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 export const http = axios.create({
-    baseURL: "http://hifl.herokuapp.com/api/v1"
+    // baseURL: "http://hifl.herokuapp.com/api/v1"
+    baseURL: process.env.REACT_APP_URL
+
 })
 
 export const privateHttp = axios.create({
-    baseURL: "http://hifl.herokuapp.com/api/v1"
-    // baseURL: process.env.REACT_APP_URL
+    // baseURL: "http://hifl.herokuapp.com/api/v1"
+    baseURL: process.env.REACT_APP_URL
 })
 
 privateHttp.interceptors.request.use((config: any) => {
